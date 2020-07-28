4eacf8b51dcf89443eec9707780a8a7b48d5ef17
unknown <MahmoudAzab-com>
2020-07-28 17:19:15 +0200

    Generate Question model via artisan model generator

---
+++ app/Question.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+    //
+    protected $fillable = ['title', 'body'];
+    public function user()
+    {
+        # code...
+        return $this->belongTo(User::class);
+    }
+    $question = Question::find(1);
+    $question -> user -> email;
+}
